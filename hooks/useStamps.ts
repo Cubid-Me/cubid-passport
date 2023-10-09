9cb4ba450076fe398f0ec8a6f0444a772ff766ae
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-10-09 13:38:10 +0530

    changes

---
+++ hooks/useStamps.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useMemo, useState } from "react"
+import axios from "axios"
+import { useAccount } from "wagmi"
+
+export const useStamps = () => {
+  const [stamps, setStamps] = useState([])
+  const { address } = useAccount()
+
+  const fetchAccount = useCallback(async () => {
+    if (address) {
+      const { data } = await axios.get(
+        `https://api.scorer.gitcoin.co/registry/stamps/${address}?include_metadata=true`,
+        {
+          headers: {
+            "X-API-KEY": "8Txcnbid.OwEP6pT0ElSptVZZfrEaKhJg8UCroukb",
+          },
+        }
+      )
+      setStamps(data?.items)
+    }
+  }, [address])
+
+  useEffect(() => {
+    fetchAccount()
+  }, [fetchAccount])
+
+  const stampCollector = useMemo(() => {
+    const stampDataArray = []
+    let counter = 0
+    for (const i of stamps as any) {
+    console.log(i);
+      let st = {
+        id: counter,
+        stamp: i?.credential?.credentialSubject?.provider,
+        icon: i?.metadata?.platform?.icon,
+      }
+      stampDataArray.push(st)
+      counter += 1
+    }
+    return stampDataArray
+  }, [stamps])
+
+  return { stamps, stampCollector }
+}
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -2,7 +2,6 @@ import { JetBrains_Mono as FontMono, Inter as FontSans } from "next/font/google"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
-  variable: "--font-sans",
 })
 
 export const fontMono = FontMono({
