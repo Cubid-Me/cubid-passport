c2b5a05cf79aeb3cde7c50c3109e3224e4f079f7
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2023-11-17 18:21:03 +0530

    changes

--- components/stamps/instagramConnect.tsx
+++ components/stamps/instagramConnect.tsx
@@ -12,7 +12,7 @@ import {
 
 import { Button } from "../ui/button"
 
-const redirectUri = "https://cubid-passport.vercel.app"
+const redirectUri = "https://cubid-passport.vercel.app/app"
 
 const InstagramAuth = () => {
   const handleLogin = () => {
