a281c6cffe4e11b4015b3a068e98366b4e17d27e
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-01-08 03:26:57 +0530

    Changes

--- pages/api/fractal.ts
+++ pages/api/fractal.ts
@@ -6,8 +6,8 @@ export default async function handler(req: any, res: any) {
     const { data } = await axios.post(
       "https://auth.next.fractal.id/oauth/token",
       {
-        client_id: "HiOtrTXl-1Racpv9pbRtt8hDZRlOvljVCjFP5LyWlnk",
-        client_secret: "c2z_IyVbx_XrPmETdPA9jhaL8XfcyLqLHNGgzK7007s",
+        client_id: "Zh7_RqOPeQoV1lZJ_ZUF1b88VGyJwaxzQVQbpvxq4S4",
+        client_secret: "vmS0iHz95domUcD8Lo7E0mt1axdU16rR4XYUTWngY1o",
         code: code,
         grant_type: "authorization_code",
         redirect_uri: "https://cubid-passport.vercel.app/",
