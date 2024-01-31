77bcdd299b781fdb349fbbee20d8bf89e667a099
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-02-01 04:34:21 +0530

    changes

--- components/web3Modal.tsx
+++ components/web3Modal.tsx
@@ -1,14 +1,12 @@
+// @ts-nocheck
 "use client"
-
 import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum"
 import { Web3Modal as Modal } from "@web3modal/react"
 import { WagmiConfig, configureChains, createConfig } from "wagmi"
 import { mainnet, optimism, polygon } from "wagmi/chains"
 
 // 1. Get projectID at https://cloud.walletconnect.com
-if (!process.env.NEXT_PUBLIC_PROJECT_ID) {
-  throw new Error("You need to provide NEXT_PUBLIC_PROJECT_ID env variable")
-}
+
 const projectId = 'a22385802b21cd5e16bdba9cbe7c7de0'
 
 // 2. Configure wagmi client
