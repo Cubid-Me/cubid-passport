859b9bcb460d38e9265f61e5f409e3c6f37dd569
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2024-02-15 03:44:16 +0530

    changes

--- components/stamps/stampFlow.tsx
+++ components/stamps/stampFlow.tsx
@@ -176,7 +176,7 @@ export const Stamps = ({
   const [instagramShow, setInstagramShow] = useState(false)
   const [stampCategories, setStampCategories] = useState([])
   const [allStamps, setAllStamps] = useState([])
-  const [email,setEmail] = useState(false)
+  const [email,setEmail] = useState("")
   const { stampCollector } = useStamps()
   const { supabaseUser, getUser } = useAuth({ appId })
 
