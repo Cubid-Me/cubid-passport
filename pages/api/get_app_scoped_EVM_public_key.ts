b234774f1d1f6e036295f4d9977cee12220618e0
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-08-13 21:22:49 +0530

    changes

--- pages/api/get_app_scoped_EVM_public_key.ts
+++ pages/api/get_app_scoped_EVM_public_key.ts
@@ -3,6 +3,10 @@ import { ethers } from "ethers"
 import NextCors from "nextjs-cors"
 
 import { supabase } from "./utils/supabase"
+import { insertStampPerm } from "@/lib/insert_stamp_perm"
+import axios from "axios"
+import { stampsWithId } from "./utils/stampKey"
+import { encode_data } from "@/lib/encode_data"
 
 export default async function handler(
   req: NextApiRequest,
@@ -25,7 +29,35 @@ export default async function handler(
   const { data, error } = await supabase
     .from("evm_accounts")
     .insert([{ private_key: privateKey, address: address, dapp_id, user_id }])
-
+  const dataToSet_stamp = {
+    created_by_user_id: user_id,
+    created_by_app: await dapp_id,
+    stamptype: stampsWithId.evm,
+    uniquevalue: address,
+    user_id_and_uniqueval: `${user_id} ${stampsWithId.evm} ${address}`,
+    unique_hash: await encode_data(address),
+    stamp_json: { address },
+    type_and_uniquehash: `${stampsWithId.evm} ${await encode_data(address)}`,
+  }
+  const {
+    data: { data: evmData },
+  } = await axios.post("/api/supabase/insert", {
+    table: "stamps",
+    body: dataToSet_stamp,
+  })
+  if (evmData?.[0]?.id) {
+    await axios.post("/api/supabase/insert", {
+      table: "authorized_dapps",
+      body: {
+        dapp_id: 22,
+        dapp_and_stamp_id: `22 ${evmData?.[0]?.id}`,
+        stamp_id: evmData?.[0]?.id,
+        can_read: true,
+        can_update: true,
+        can_delete: true,
+      },
+    })
+  }
   if (error) {
     return res.status(500).json({ error: error.message })
   }
