fb20e387469e69d1fac6aacf2c996d629db6871a
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-08-16 02:07:29 +0530

    changes

--- app/pii/page.tsx
+++ app/pii/page.tsx
@@ -507,6 +507,7 @@ export default function IndexPage() {
               id="phone"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               {...register("phone", { required: true })}
+              value={watch("phone")}
             >
               <option value="">Choose a Phone</option>
               {phoneFields.map((field, index) => (
@@ -547,6 +548,7 @@ export default function IndexPage() {
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               {...register("email", { required: true })}
+              value={watch("email")}
             >
               <option value="">Choose an Email</option>
               {emailFields.map((field, index) => (
