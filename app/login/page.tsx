a7944a56ab7e7ccf3717394687a866b2a940be0d
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-11-04 09:40:13 +0530

    changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,6 +12,8 @@ import { toast } from "react-toastify"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import PhoneInput from "react-phone-input-2"
+import "react-phone-input-2/lib/style.css"
 
 function debounce(func: any, delay: any) {
   let timeoutId: any
@@ -29,31 +31,78 @@ export default function AuthenticationPage() {
   const passwordField = useRef(null)
   const [isEnabled, setIsEnabled] = useState(true)
   const [loading, setLoading] = useState(false)
+
+  // New states for phone authentication
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [otp, setOtp] = useState("")
+  const [isOtpSent, setIsOtpSent] = useState(false)
+  const [verificationId, setVerificationId] = useState(null)
+  const recaptchaVerifier = useRef(null);
+
   const submit = async (values: any) => {
     try {
       localStorage.setItem("email", emailField.current.value)
-
-      const {
-        data: { data },
-      } = await axios.post("/api/supabase/select", {
+      const { data: { data } } = await axios.post("/api/supabase/select", {
         match: { email: emailField.current.value },
         table: "users",
       })
       await firebase.auth().signInWithCustomToken(values.idToken)
-
       if (!data?.[0]) {
         await axios.post(`/api/supabase/insert`, {
           table: "users",
           body: { email: localStorage.getItem("email") },
         })
       }
-      // axios.post('/api/login-create-user',{
-      //   email
-      // })
       toast.success("Successfully logged into cubid")
     } catch (err) {
       console.error(err)
-      toast.error("An error while authenticating user")
+      toast.error("An error occurred while authenticating user")
+    }
+  }
+
+
+
+
+  const sendOtp = async () => {
+    setLoading(true)
+    let appVerifier = new firebase.auth.RecaptchaVerifier("catcha-id", {
+      size: "normal", // Set to 'normal' for testing purposes to see the widget
+      callback: (response) => {
+        console.log(response)
+        // reCAPTCHA solved - allow OTP to be sent
+      },
+      "expired-callback": () => {
+        // Handle reCAPTCHA expiration
+        recaptchaVerifier.current.reset();
+      },
+    });
+    try {
+      const confirmationResult = await firebase.auth().signInWithPhoneNumber(
+        phoneNumber,
+        appVerifier
+      );
+      
+      setVerificationId(confirmationResult.verificationId)
+      setIsOtpSent(true)
+      toast.success("OTP sent successfully")
+    } catch (err) {
+      console.error(err)
+      toast.error("Error sending OTP")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const verifyOtp = async () => {
+    if (verificationId && otp) {
+      const credential = firebase.auth.PhoneAuthProvider.credential(verificationId, otp)
+      try {
+        await firebase.auth().signInWithCredential(credential)
+        toast.success("Phone authentication successful")
+      } catch (err) {
+        console.error(err)
+        toast.error("Invalid OTP")
+      }
     }
   }
 
@@ -83,7 +132,7 @@ export default function AuthenticationPage() {
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                {`"Welcome to the cubid passort, proof of humanity on web3 and the new internet"`}
+                {`"Welcome to the cubid passport, proof of humanity on web3 and the new internet"`}
               </p>
               <footer className="text-sm">Noak</footer>
             </blockquote>
@@ -113,9 +162,46 @@ export default function AuthenticationPage() {
               ref={passwordField}
               placeholder="password"
             />
+
+            {/* Phone Authentication Fields with react-phone-input-2 */}
+            <PhoneInput
+              country={"us"}
+              inputClass="!text-black"
+              value={phoneNumber}
+              onChange={(phone) => setPhoneNumber(`+${phone}`)}
+              inputProps={{
+                name: "phone",
+                required: true,
+                autoFocus: true,
+              }}
+              placeholder="Phone Number"
+            />
+            {isOtpSent && (
+              <Input
+                type="text"
+                placeholder="Enter OTP"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+              />
+            )}
+            <div id="catcha-id"></div>
+            <Button
+              id="send-otp-button"
+              onClick={sendOtp}
+              disabled={loading || isOtpSent}
+            >
+              Send OTP
+            </Button>
+            {isOtpSent && (
+              <Button onClick={verifyOtp} disabled={loading}>
+                Verify OTP
+              </Button>
+            )}
+
             <div style={{ width: 30, textAlign: "center", paddingLeft: "4px" }}>
               {loading && (
                 <div>
+                  {/* Loading Spinner */}
                   <svg
                     aria-hidden="true"
                     className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
@@ -123,18 +209,12 @@ export default function AuthenticationPage() {
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
+                    {/* SVG paths */}
                   </svg>
                 </div>
               )}
             </div>
+
             {!isEnabled && (
               <button
                 style={{
@@ -154,6 +234,7 @@ export default function AuthenticationPage() {
                 Continue
               </button>
             )}
+
             {!loading && isEnabled && (
               <div
                 style={
@@ -180,6 +261,7 @@ export default function AuthenticationPage() {
             )}
           </div>
         </div>
+
       </div>
     </Guest>
   )
