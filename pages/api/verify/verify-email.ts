7868b181306e689e4e9bdf5327a4f810a55e3035
Harjaap Dhillon <harvydhillon16@gmail.com>
2024-11-05 17:00:15 +0530

    changes

--- pages/api/verify/verify-email.ts
+++ pages/api/verify/verify-email.ts
@@ -9,7 +9,7 @@ const verifyEmail = async (req: any, res: any) => {
         origin: "*", // Allow all origins
         optionsSuccessStatus: 200, // Some legacy browsers choke on 204
     })
-    const { apikey, email, otp } = typeof req.body === "string" ? JSON.parse(req.body) : req.body
+    const { apikey, email, otp, allStamps, dappuser_id } = typeof req.body === "string" ? JSON.parse(req.body) : req.body
     const { data: dataForApp } = await supabase
         .from("dapps")
         .select("*")
@@ -18,15 +18,31 @@ const verifyEmail = async (req: any, res: any) => {
     if (!dappId) {
         return res.status(400).json({ error: "Invalid API key" })
     }
+
     const { data } = await supabase.auth.verifyOtp({
         email,
         token: otp,
         type: "email"
     })
 
+    const allPromises = allStamps.map((item: any) =>
+        supabase
+            .from("stamp_dappuser_permissions")
+            .insert({
+                dappuser_id: dappuser_id,
+                stamp_id: item.id,
+                can_write: true,
+                can_delete: true,
+                can_read: true,
+            })
+    );
+    await Promise.all(allPromises);
+
+
     res.send({
         success: Boolean(data?.user?.id),
     })
+
 }
 
 export default verifyEmail
