6e14f598f4212af8c7c88e18d3bf2056d2a3fef2
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Air.local>
2025-01-31 15:36:56 +0530

    Change

--- pages/api/utils/stampKey.ts
+++ pages/api/utils/stampKey.ts
@@ -1,23 +1,26 @@
 
-export const stampsWithId:any = {
-    facebook: 1,
-    github: 2,
-    google: 3,
-    twitter: 4,
-    discord: 5,
-    poh: 6,
-    email:13,
-    "near-wallet": 7,
-    brightid: 8,
-    gitcoin: 9,
-    instagram: 10,
-    phone: 11,
-    gooddollar: 12,
-    fractal: 17,
-    linkedin: 22,
-    worldcoin: 26,
-    telegram: 27,
-    evm:14,
-    solana:53
-  }
-  
+export const stampsWithId: any = {
+  facebook: 1,
+  github: 2,
+  google: 3,
+  twitter: 4,
+  discord: 5,
+  poh: 6,
+  iah: 7,
+  brightid: 8,
+  gitcoin: 9,
+  instagram: 10,
+  phone: 11,
+  gooddollar: 12,
+  "near-wallet": 15,
+  fractal: 17,
+  evm: 14,
+  email: 13,
+  solana: 53,
+  telegram: 27,
+  worldcoin: 26,
+  near: 15,
+  "lens-protocol": 66,
+  farcaster: 68,
+  address: 70
+}
