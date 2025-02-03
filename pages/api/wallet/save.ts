5039eedd5a1e2323d9849f895febef18c4de8626
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Air.local>
2025-02-03 20:19:14 +0530

    changes

--- pages/api/wallet/save.ts
+++ pages/api/wallet/save.ts
@@ -1,3 +1,4 @@
+import { server_insertStamp } from "@/lib/stampInsertion";
 import { supabase } from "@/lib/supabase"
 import axios from "axios"
 import NextCors from "nextjs-cors";
@@ -17,6 +18,18 @@ export default async function handler(req: any, res: any) {
             public_key,
             is_generated_via_lib
         }).select("*")
+        if (!error) {
+            const { data } = await supabase.from("dapp_users").select("*").match({ uuid: dapp_uid })
+            server_insertStamp({
+                user_data: { user_id: data?.[0]?.user_id, uuid: '' },
+                stampData: {
+                    identity: public_key,
+                    uniquevalue: public_key,
+                },
+                stamp_type: chain === "near" ? 'near-wallet' : "evm",
+                app_id: data?.[0]?.dapp_id
+            })
+        }
         console.log({ data, error })
         res.send({ data, error })
     } catch (err: any) {
